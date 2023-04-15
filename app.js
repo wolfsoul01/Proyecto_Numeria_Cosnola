774f9907300e9f9aeda6cb64e304cac3f678f54b
wolfsoul <alejandrof011021@gmail.com>
2023-04-14 22:16:04 -0400

    Se crearon los comando de ejecucion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import colors from "colors";
 
-import {mostrarMenu,menuBiseccion} from "./modules/inquire_menu.js ";
+import { mostrarMenu, menuBiseccion } from "./modules/inquire_menu.js ";
 import pausa from "./modules/pausa.js";
 import { funcion } from "./algoritmos/funcion.js";
 import { crearFuncion, evaluarFuncion } from "./algoritmos/evaluar_polin.js";
@@ -9,9 +9,8 @@ import { number } from "mathjs";
 import { mostrarLogo } from "./modules/ascii.js";
 
 const Main = async () => {
-  
   let opt = "";
-  
+
   do {
     opt = await mostrarMenu();
     //console.log(opt);
@@ -22,19 +21,17 @@ const Main = async () => {
       const { f, x } = await funcion();
       console.log("La funcion es: ", `${f}`.gray);
       console.log("La evalucacion es: ", evaluarFuncion(f, Number(x)));
-    }
-    else if(opt=2){
-      const {p,a,b,error}= await menuBiseccion();
-      const f=  crearFuncion(p);
+    } else if ((opt = 2)) {
+      const { p, a, b, error } = await menuBiseccion();
+      const f = crearFuncion(p);
       console.log(p);
       console.log(a);
       console.log(b);
       console.log(error);
       console.log(f);
 
-      await biseccion(f,Number(a),Number(b),error)
+      await biseccion(f, Number(a), Number(b), error);
     }
-
 
     await pausa();
   } while (opt !== "3");
@@ -42,4 +39,4 @@ const Main = async () => {
 
 mostrarLogo();
 
-setTimeout(Main,5000)
+setTimeout(Main, 5000);
